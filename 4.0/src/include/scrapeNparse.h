034246e91e37d87317035f33be3a55f4def7f7d8
fobboyandy <fobboyandy@gmail.com>
2016-07-03 13:09:55 -0500

    added srapeNparse class

---
+++ 4.0/src/include/scrapeNparse.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <curl.h>
+#include <unordered_map>
+using namespace std;
+
+class scrapeNparse
+{
+	typedef string URL;
+
+public:
+	scrapeNparse() 
+	{
+
+	}
+	~scrapeNparse()
+	{
+
+	}
+	string scrape(URL url_link)
+	{
+
+	}
+private:
+	string content;
+	fstream data;
+};
